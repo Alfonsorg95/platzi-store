63d792e8ef8938de143901601bf4c711dbfb2086
Alfonso Rodriguez <alfonsorg95@gmail.com>
2023-01-22 19:39:14 +0100

    Enviroments config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,26 @@
 import { Module } from '@nestjs/common';
 import { HttpModule, HttpService } from '@nestjs/axios';
 import { firstValueFrom } from 'rxjs';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
 import { DatabaseModule } from './database/database.module';
 
+import { enviroments } from './enviroment';
+
 @Module({
-  imports: [UsersModule, ProductsModule, HttpModule, DatabaseModule],
+  imports: [
+    UsersModule,
+    ProductsModule,
+    HttpModule,
+    DatabaseModule,
+    ConfigModule.forRoot({
+      envFilePath: enviroments[process.env.NODE_ENV] || '.env',
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [
     AppService,
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,11 +1,17 @@
 import { Injectable, Inject } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
-  constructor(@Inject('Task') private task: any[]) {}
+  constructor(
+    @Inject('Task') private task: any[],
+    private config: ConfigService,
+  ) {}
 
   getHello(): string {
-    console.log(this.task);
-    return 'Hello World!';
+    const apiKey = this.config.get('API_KEY');
+    const database = this.config.get('DATABASE_NAME');
+    //console.log(this.task);
+    return `Hello World! API: ${apiKey}, DB: ${database}`;
   }
 }
