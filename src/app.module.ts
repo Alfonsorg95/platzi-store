c4d10969df10850c290a1c9dc5850653a8ff3bfa
Alfonso Rodriguez <alfonsorg95@gmail.com>
2023-01-22 18:49:36 +0100

    Dependencies injection and global modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,29 @@
 import { Module } from '@nestjs/common';
+import { HttpModule, HttpService } from '@nestjs/axios';
+import { firstValueFrom } from 'rxjs';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [UsersModule, ProductsModule],
+  imports: [UsersModule, ProductsModule, HttpModule, DatabaseModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: 'Task',
+      useFactory: async (http: HttpService) => {
+        const task = await http.get(
+          'https://jsonplaceholder.typicode.com/todos',
+          { headers: { 'Accept-Encoding': 'gzip,deflate,compress' } },
+        );
+        const value = Promise.resolve(firstValueFrom(task));
+        return value;
+      },
+      inject: [HttpService],
+    },
+  ],
 })
 export class AppModule {}
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Inject } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+  constructor(@Inject('Task') private task: any[]) {}
+
   getHello(): string {
+    console.log(this.task);
     return 'Hello World!';
   }
 }
